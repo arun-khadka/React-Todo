2b4587837a9cb56cb2956420dea397c7dcbb735d
arunKhadka <arunkhadka009@gmail.com>
2023-12-30 18:31:19 +0545

    react todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,210 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+// import {AiOutlineDelete} from 'react-icons/ai';
+// import {BsCheckLg} from 'react-icons/bs';
+// import './App.css'
 
-function App() {
-  const [isCompleteScreen, setIsCompleteScreen] = useState(false)
+// const MultipleInputs = () => {
+//   const [allTodos, setTodos] = useState([]);
+  
+//   const [newTitle, setNewTitle] = useState("");
+//   const [newDescritpion, setNewDescription] = useState("");
+//   const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+  
 
+//   const handleAddTodo = (e) => {
+//     const newTodo = {
+//       id:Date.now(),
+//       Title: '',
+//       Description: '',
+//     }
+//     setTodos([...allTodos, newTodo]);
+//     };
 
-  return (
-    <div className='App'> 
-       <h2>Todo List</h2> 
+//   const handleSubmit = (index) => {
+//     return(e) => {
+//       e.preventDefault();
+//       console.log(allTodos[index]);
+//       setNewTitle(allTodos[index].Title);
+//       setNewDescription(allTodos[index].Description);
+//     };
+//   };
 
-      <div className='todo-wrapper'>
-         <div className='todo-input'>
+//   return (
+    // <div className='App'> 
+    //    <h2>Todo List</h2> 
+
+    //   <div className='todo-wrapper'>
+    //      <div className='todo-input'>
           
-          <div className='todo-input-item'>
+    //       <div className='todo-input-item'>
+    //         <label htmlFor='title-input'>Title</label>
+    //          <input 
+    //           id='title-input'
+    //           type='text' 
+    //           value={allTodos[allTodos.length - 1]?.Title} 
+    //           onChange={(e) => {
+    //             const updatedTodo = {...allTodos[allTodos.length - 1], Title: e.target.value};
+    //             handleAddTodo(updatedTodo);
+    //           }} 
+    //           name='Title'/>
+    //       </div>
+
+    //       <div className='todo-input-item'>
+    //         <label htmlFor='description-input'>Description</label>
+    //          <input 
+    //           id='description-input'
+    //           type='text' 
+    //           value={allTodos[allTodos.length - 1]?.Description} 
+    //           onChange={(e) => {
+    //             const updatedTodo = {...allTodos[allTodos.length - 1], Description: e.target.value};
+    //             handleAddTodo(updatedTodo);
+    //           }} 
+    //           name='Description'/>
+    //       </div>
+
+    //       <div className='todo-input-item'>
+    //         <button 
+    //           type='button' 
+    //           onClick={() => {
+    //             const updatedTodo = {...allTodos[allTodos.length - 1], Title: '', Description: ''};
+    //             handleAddTodo(updatedTodo)
+    //           }} 
+    //           className='primaryBtn'>Add
+    //         </button>
+    //       </div>
+    //     </div>
+
+    //     <div className='btn-area'>
+    //       <button 
+    //       className={`secondaryBtn ${isCompleteScreen===false} && 'active'}`} 
+    //       onClick={() => isCompleteScreen(true)}>Todo
+    //       </button>
+
+    //       <button 
+    //       className={`secondaryBtn ${isCompleteScreen===true} && 'active'}`} 
+    //       onClick={() => isCompleteScreen(false)}>Completed
+    //       </button>
+          
+    //     </div>
+
+    //     <div className='todo-list'>
+    //       {allTodos.map((todo, index) => (
+    //         <div className='todo-list-item' key={todo.id}>
+    //         <h3>{todo.Title}</h3>
+    //         <p>{todo.Descritpion}</p>
+    //       </div>
+    //       ))}
+          
+        
+    //       <div>
+    //         <AiOutlineDelete className='icon'/>
+    //         <BsCheckLg className='check-icon'/>
+    //       </div>  
+    //     </div> 
+    //   </div>  
+    // </div>
+//  )
+
+// }
+
+
+// export default MultipleInputs
+
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import {AiOutlineDelete} from 'react-icons/ai';
+import {BsCheckLg} from 'react-icons/bs';
+
+function App () {
+    const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+    const [allTodos, setTodos] = useState([]);
+    const [newDescription, setNewDescription] = useState("");
+    const [newTitle, setNewTitle] = useState("");
+
+    const handleAddTodo = () => {
+        let newTodoItem = {
+            title: newTitle,
+            description: newDescription
+        }
+
+        let updatedTodoArr = [...allTodos];
+        updatedTodoArr.push(newTodoItem);
+        setTodos(updatedTodoArr);
+    }
+
+    return(
+
+        <div className='App'> 
+        <h2>Todo List</h2> 
+
+        <div className='todo-wrapper'>
+        <div className='todo-input'>
+        
+        <div className='todo-input-item'>
             <label>Title</label>
-            <input type='text' className='form-input' placeholder=''/>
-          </div>
+            <input 
+            type='text' 
+            value={newTitle} 
+            onChange={(e) => 
+                setNewTitle(e.target.value)}
+                />
+        </div>
 
-          <div className='todo-input-item'>
+        <div className='todo-input-item'>
             <label>Description</label>
-            <input type='text' className='form-input' placeholder=''/>
-          </div>
 
-          <div className='todo-input-item'>
-            <button type='button' className='primaryBtn'>Add</button>
-          </div>
+            <input 
+            type='text' 
+            value={newDescription} 
+            onChange={(e) => 
+            setNewDescription(e.target.value)}
+            />
+        </div>
+
+        <div className='todo-input-item'>
+            <button 
+            type='button' 
+            onClick={handleAddTodo} 
+            className='primaryBtn'>Add
+            </button>
+        </div>
+
         </div>
 
         <div className='btn-area'>
-          <button 
-          className={`secondaryBtn ${isCompleteScreen===false} && 'active'}`} 
-          onClick={() => isCompleteScreen(true)}>Todo
-          </button>
+        <button 
+        className={`secondaryBtn ${isCompleteScreen===false} && 'active'}`} 
+        onClick={() => isCompleteScreen(true)}>Todo
+        </button>
 
-          <button 
-          className={`secondaryBtn ${isCompleteScreen===true} && 'active'}`} 
-          onClick={() => isCompleteScreen(false)}>Completed
-          </button>
-          
+        <button 
+        className={`secondaryBtn ${isCompleteScreen===true} && 'active'}`} 
+        onClick={() => isCompleteScreen(false)}>Completed
+        </button>
+        
         </div>
 
         <div className='todo-list'>
-          <div className='todo-list-item'>
-            <h3>Task 1</h3>
-            <p>This is task 1</p>
-          </div>  
+            {allTodos.map((item, index)=>{
+                return(
+                    <div className='todo-list-item' key={index}>
+                        <div>
+                            <h3>{item.title}</h3>
+                            <p>{item.description}</p>
+                        </div>
+        
+                        <div>
+                            <AiOutlineDelete className='icon'/>
+                            <BsCheckLg className='check-icon'/>
+                        </div>
+                        
+                    </div>    
+                )
+            })}    
+        
         </div> 
-
       </div>  
-    </div>
- )
-
+     </div>
+    )
 }
-
-export default App
+export default App;
